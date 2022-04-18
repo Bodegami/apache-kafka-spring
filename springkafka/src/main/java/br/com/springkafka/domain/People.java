e31950f241ee6a12a5cb37b70a517ca168384839
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-04-18 00:25:42 -0300

    add PeopleRepository & refatoracao do people consumer

--- springkafka/src/main/java/br/com/springkafka/domain/People.java
+++ springkafka/src/main/java/br/com/springkafka/domain/People.java
@@ -1,13 +1,17 @@
 package br.com.springkafka.domain;
 
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 @Entity
 public class People {
 
@@ -18,7 +22,7 @@ public class People {
     private String name;
     private String cpf;
 
-    @OneToMany(mappedBy = "people")
+    @OneToMany(mappedBy = "people", cascade = CascadeType.ALL)
     private List<Book> books;
 
 }
