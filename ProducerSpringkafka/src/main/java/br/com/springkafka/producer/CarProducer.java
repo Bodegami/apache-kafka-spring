2222d913231f3bb09c572c5353dfb173f67912e1
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2022-04-19 23:32:41 -0300

    criando um novo Avro, Controller, DTO e Producer

---
+++ ProducerSpringkafka/src/main/java/br/com/springkafka/producer/CarProducer.java
@@ -0,0 +1,29 @@
+package br.com.springkafka.producer;
+
+import br.com.springkafka.Car;
+import br.com.springkafka.People;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CarProducer {
+
+    private final String topicName;
+    private final KafkaTemplate<String, Car> kafkaTemplate;
+
+    public CarProducer(@Value("${topic.name}") String topicName, KafkaTemplate<String, Car> kafkaTemplate) {
+        this.topicName = topicName;
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(Car car) {
+        kafkaTemplate.send(topicName, (String) car.getId(), car).addCallback(
+                sucess -> log.info("Mensagem enviada com sucesso!"),
+                falure -> log.error("Falha ao enviar mensagem!")
+        );
+    }
+
+}
